3a7c0c0f7581af64734cfee3e9b2743f8531ff6f
jase311 <92610397+jase311@users.noreply.github.com>
2022-09-19 11:54:36 -0500

    Update Energy_Transition.py

--- Energy_Transition.py
+++ Energy_Transition.py
@@ -14,7 +14,7 @@ pd.set_option('display.width', 32000)
 # Create the necessary dataframes: Production, Consumption and GDP
 df_consumption = pd.read_csv(r'https://github.com/jase311/Energy_Transition/blob/main/modern-renewable-energy-consumption.csv',on_bad_lines='skip')
 df_production = pd.read_csv(r'https://github.com/jase311/Energy_Transition/blob/main/modern-renewable-prod.csv',on_bad_lines='skip')
-df_gdp = pd.read_excel(r'https://github.com/jase311/Energy_Transition/blob/main/GDP%20year%20country.xlsx',on_bad_lines='skip')
+df_gdp = pd.read_excel(r'https://github.com/jase311/Energy_Transition/blob/main/GDP%20year%20country.xlsx')
 
 
 
