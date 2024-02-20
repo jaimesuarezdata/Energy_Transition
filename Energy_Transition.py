49c82c3eec3cfa891304333cfbb0bf0912d51111
jase311 <92610397+jase311@users.noreply.github.com>
2024-02-20 17:46:43 -0500

    Update Energy_Transition.py

--- Energy_Transition.py
+++ Energy_Transition.py
@@ -3,7 +3,7 @@ import numpy as np
 import streamlit as st
 import plotly_express as px
 import plotly.graph_objects as go
-# import openpyxl
+import openpyxl
 
 # Set visualization options
 pd.set_option('display.max_columns',200)
